885b1ce371a9c85c0ac13d0e8ad9e070735e303b
amlxv <amirulfcso@gmail.com>
2023-08-04 21:26:32 +0800

    Configure prettier & its plugin for Tailwind CSS

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="p-4 bg-blue-200 m-4 border border-blue-500">
+    <div class="m-4 border border-blue-500 bg-blue-200 p-4">
         This will be view for an index / landing page.
     </div>
 </template>
