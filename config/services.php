0c814e70cacea504da5633b03015d017f8578a91
amlxv <amirulfcso@gmail.com>
2023-08-08 15:12:06 +0800

    Scaffold GitHub social provider integration

--- config/services.php
+++ config/services.php
@@ -36,4 +36,10 @@ return [
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_AUTH_REDIRECT'),
     ],
+
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('GITHUB_AUTH_REDIRECT'),
+    ],
 ];
