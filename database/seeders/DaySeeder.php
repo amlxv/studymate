1694b6a72a23f6cca3184443d717e306048275cb
amlxv <amirulfcso@gmail.com>
2023-08-12 22:56:04 +0800

    WIP

---
+++ database/seeders/DaySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Day;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DaySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+        foreach ($days as $day) {
+            Day::create(['name' => $day]);
+        }
+
+    }
+}
