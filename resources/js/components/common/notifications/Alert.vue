018476cda653fda9daa7f65a4e95ceab4443c1ca
amlxv <amirulfcso@gmail.com>
2023-09-04 20:16:39 +0800

    WIP

--- resources/js/components/common/notifications/Alert.vue
+++ resources/js/components/common/notifications/Alert.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { XMarkIcon } from "@heroicons/vue/20/solid";
+import { defineProps } from "vue";
 import { sanitizeNotification, Type } from "@/types/notifications";
 
 const props = defineProps<{
@@ -26,17 +26,6 @@ console.log(status);
                 <p class="text-sm font-medium" :class="status?.textColor">
                     {{ status?.message }}
                 </p>
-            </div>
-            <div class="ml-auto pl-3">
-                <div class="-mx-1.5 -my-1.5">
-                    <button
-                        type="button"
-                        class="inline-flex rounded-md bg-gray-50 p-1.5 text-gray-500 opacity-50 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2 focus:ring-offset-gray-50"
-                    >
-                        <span class="sr-only">Dismiss</span>
-                        <XMarkIcon class="h-5 w-5" aria-hidden="true" />
-                    </button>
-                </div>
             </div>
         </div>
     </div>
