ea58430ebd2b101ac1545bd8a40ad59a1a3b73bc
amlxv <amirulfcso@gmail.com>
2023-10-01 07:11:21 +0800

    WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,8 +8,8 @@ const pinia = createPinia();
 
 createInertiaApp({
     resolve: (name) => {
-        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
-        return pages[`./Pages/${name}.vue`];
+        const pages = import.meta.glob("./pages/**/*.vue", { eager: true });
+        return pages[`./pages/${name}.vue`];
     },
     setup({ el, App, props, plugin }) {
         const app = createApp({ render: () => h(App, props) })
