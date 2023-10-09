a5eca3f4540c79a837c8458d103b136303b8851d
amlxv <amirulfcso@gmail.com>
2023-10-09 23:56:04 +0800

    WIP

--- resources/js/components/schedules/InfoModal.vue
+++ resources/js/components/schedules/InfoModal.vue
@@ -111,13 +111,18 @@ const { open, event } = toRefs(props);
                                     >
                                         Close
                                     </button>
-                                    <button
+                                    <Link
+                                        :href="
+                                            $route(
+                                                'schedule.edit',
+                                                event?.['id'],
+                                            )
+                                        "
                                         type="button"
-                                        class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                                        @click="open = false"
+                                        class="mt-3 inline-flex w-full cursor-pointer justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                                     >
                                         Edit
-                                    </button>
+                                    </Link>
                                 </div>
 
                                 <p class="mt-4 block text-xs text-gray-400">
