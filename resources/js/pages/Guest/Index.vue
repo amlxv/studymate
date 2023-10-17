b43b4bd8906795034ef9259f55c14146b285f969
amlxv <amirulfcso@gmail.com>
2023-10-17 08:43:31 +0800

    WIP

--- resources/js/pages/Guest/Index.vue
+++ resources/js/pages/Guest/Index.vue
@@ -14,10 +14,9 @@ import {
 const page = usePage();
 
 const navigations = [
-    { name: "Product", href: "#" },
     { name: "Features", href: "#features" },
-    { name: "About", href: "#" },
-    { name: "Contact", href: "#" },
+    { name: "About", href: "#about" },
+    { name: "Contact", href: "#contact" },
 ];
 
 const mobileMenuOpen = ref(false);
@@ -51,7 +50,7 @@ const features = [
 </script>
 
 <template>
-    <div class="bg-white" id="">
+    <div class="bg-white">
         <!-- Header -->
         <header class="absolute inset-x-0 top-0 z-50">
             <nav
@@ -178,7 +177,7 @@ const features = [
                         "
                     />
                 </div>
-                <div class="py-24 sm:py-60">
+                <div class="py-24 sm:pb-20 sm:pt-60">
                     <div class="mx-auto max-w-7xl px-6 lg:px-8">
                         <div class="mx-auto max-w-2xl text-center">
                             <h1
@@ -269,7 +268,10 @@ const features = [
             </div>
 
             <!-- Feature section -->
-            <div id="features" class="mx-auto max-w-7xl px-6 pb-60 lg:px-8">
+            <div
+                id="features"
+                class="mx-auto flex min-h-screen max-w-7xl flex-col justify-center px-6 lg:px-8"
+            >
                 <div class="mx-auto max-w-2xl lg:text-center">
                     <h2
                         class="text-base font-semibold leading-7 text-indigo-600"
@@ -317,6 +319,203 @@ const features = [
                             </dd>
                         </div>
                     </dl>
+                </div>
+            </div>
+
+            <!-- Contact section -->
+            <div
+                id="contact"
+                class="mx-auto flex min-h-screen max-w-7xl flex-col justify-center px-6 lg:px-8"
+            >
+                <div
+                    class="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8"
+                >
+                    <svg
+                        class="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+                        aria-hidden="true"
+                    >
+                        <defs>
+                            <pattern
+                                id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
+                                width="200"
+                                height="200"
+                                x="50%"
+                                y="-64"
+                                patternUnits="userSpaceOnUse"
+                            >
+                                <path d="M100 200V.5M.5 .5H200" fill="none" />
+                            </pattern>
+                        </defs>
+                        <svg
+                            x="50%"
+                            y="-64"
+                            class="overflow-visible fill-gray-50"
+                        >
+                            <path
+                                d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M299.5 800h201v201h-201Z"
+                                stroke-width="0"
+                            />
+                        </svg>
+                        <rect
+                            width="100%"
+                            height="100%"
+                            stroke-width="0"
+                            fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
+                        />
+                    </svg>
+                    <div class="mx-auto max-w-xl lg:max-w-4xl">
+                        <h2
+                            class="text-4xl font-bold tracking-tight text-gray-900"
+                        >
+                            Let’s talk.
+                        </h2>
+                        <p class="mt-2 text-lg leading-8 text-gray-600">
+                            We help companies and individuals build out their
+                            brand guidelines.
+                        </p>
+                        <div
+                            class="mt-16 flex flex-col gap-16 sm:gap-y-20 lg:flex-row"
+                        >
+                            <form action="#" method="POST" class="lg:flex-auto">
+                                <div
+                                    class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2"
+                                >
+                                    <div>
+                                        <label
+                                            for="first-name"
+                                            class="block text-sm font-semibold leading-6 text-gray-900"
+                                            >First name</label
+                                        >
+                                        <div class="mt-2.5">
+                                            <input
+                                                type="text"
+                                                name="first-name"
+                                                id="first-name"
+                                                autocomplete="given-name"
+                                                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <label
+                                            for="last-name"
+                                            class="block text-sm font-semibold leading-6 text-gray-900"
+                                            >Last name</label
+                                        >
+                                        <div class="mt-2.5">
+                                            <input
+                                                type="text"
+                                                name="last-name"
+                                                id="last-name"
+                                                autocomplete="family-name"
+                                                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <label
+                                            for="budget"
+                                            class="block text-sm font-semibold leading-6 text-gray-900"
+                                            >Budget</label
+                                        >
+                                        <div class="mt-2.5">
+                                            <input
+                                                id="budget"
+                                                name="budget"
+                                                type="text"
+                                                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <label
+                                            for="website"
+                                            class="block text-sm font-semibold leading-6 text-gray-900"
+                                            >Website</label
+                                        >
+                                        <div class="mt-2.5">
+                                            <input
+                                                type="url"
+                                                name="website"
+                                                id="website"
+                                                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div class="sm:col-span-2">
+                                        <label
+                                            for="message"
+                                            class="block text-sm font-semibold leading-6 text-gray-900"
+                                            >Message</label
+                                        >
+                                        <div class="mt-2.5">
+                                            <textarea
+                                                id="message"
+                                                name="message"
+                                                rows="4"
+                                                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mt-10">
+                                    <button
+                                        type="submit"
+                                        class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        Let’s talk
+                                    </button>
+                                </div>
+                                <p class="mt-4 text-sm leading-6 text-gray-500">
+                                    By submitting this form, I agree to the
+                                    <a
+                                        href="#"
+                                        class="font-semibold text-indigo-600"
+                                        >privacy&nbsp;policy</a
+                                    >.
+                                </p>
+                            </form>
+                            <div class="lg:mt-6 lg:w-80 lg:flex-none">
+                                <img
+                                    class="h-12 w-auto"
+                                    src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg"
+                                    alt=""
+                                />
+                                <figure class="mt-10">
+                                    <blockquote
+                                        class="text-lg font-semibold leading-8 text-gray-900"
+                                    >
+                                        <p>
+                                            “Lorem ipsum dolor sit amet
+                                            consectetur adipisicing elit. Nemo
+                                            expedita voluptas culpa sapiente
+                                            alias molestiae. Numquam corrupti in
+                                            laborum sed rerum et corporis.”
+                                        </p>
+                                    </blockquote>
+                                    <figcaption class="mt-10 flex gap-x-6">
+                                        <img
+                                            src="https://avatars.githubusercontent.com/u/34534953?v=4"
+                                            alt=""
+                                            class="h-12 w-12 flex-none rounded-full bg-gray-50"
+                                        />
+                                        <div>
+                                            <div
+                                                class="text-base font-semibold text-gray-900"
+                                            >
+                                                Amirul
+                                            </div>
+                                            <div
+                                                class="text-sm leading-6 text-gray-600"
+                                            >
+                                                CEO of StudyMate
+                                            </div>
+                                        </div>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
