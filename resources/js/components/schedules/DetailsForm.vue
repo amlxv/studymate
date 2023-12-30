004a3ed7b08dd2a82aff161ba5149f996d77fb40
amlxv <amirulfcso@gmail.com>
2023-12-30 22:54:48 +0800

    fix: ui on mobile

--- resources/js/components/schedules/DetailsForm.vue
+++ resources/js/components/schedules/DetailsForm.vue
@@ -160,7 +160,7 @@ const { form, type } = toRefs(props);
 
             <div class="col-span-full mt-5">
                 <SwitchGroup as="div" class="flex items-center justify-between">
-                    <span class="flex flex-grow flex-col">
+                    <span class="mr-6 flex flex-grow flex-col md:mr-auto">
                         <SwitchLabel
                             as="span"
                             class="text-sm font-medium leading-6 text-gray-900"
